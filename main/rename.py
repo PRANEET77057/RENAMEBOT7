6ffbaa8adf7c133727aba80975a4ea03f9a82e77
PRANEET77057 <140240880+PRANEET77057@users.noreply.github.com>
2023-07-22 17:41:27 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -29,20 +29,7 @@ async def rename_start(client, message):
             reply_markup=InlineKeyboardMarkup(button)
         )
 
-@Client.on_message(filters.private & filters.command("rename") & filters.user(ADMIN))             
-async def rename_file(bot, msg):
-    reply = msg.reply_to_message
-    if len(msg.command) < 2 or not reply:
-       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
-    media = reply.document or reply.audio or reply.video
-    if not media:
-       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
-    og_media = getattr(reply, reply.media.value)
-    new_name = msg.text.split(" ", 1)[1]
-    sts = await msg.reply_text("Trying to Downloading.....")
-    c_time = time.time()
-    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
-    filesize = humanbytes(og_media.file_size)                
+           
     if CAPTION:
         try:
             cap = CAPTION.format(file_name=new_name, file_size=filesize)
