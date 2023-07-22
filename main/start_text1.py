ab5c1f32e793b8f54d5d5f5b5cbd3e8d125b69d6
PRANEET77057 <140240880+PRANEET77057@users.noreply.github.com>
2023-07-22 18:07:26 +0530

    Create start_text1.py

---
+++ main/start_text1.py
@@ -0,0 +1,65 @@
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+from config import ADMIN
+ 
+
+@Client.on_message(filters.command("start") & filters.private)                             
+async def start_cmd(bot, msg):
+    txt="This is personal use bot 🙏. Do you want your own bot? 👇 Click the source code to deploy"
+    btn = InlineKeyboardMarkup([[
+        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/MrMKN/Simple-Rename-Bot")
+        ],[
+        InlineKeyboardButton("🖥️ How To Deploy", url="https://youtu.be/oc847WvOUaI")
+    ]])
+    if msg.from_user.id != ADMIN:
+        return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
+    await start(bot, msg, cb=False)
+
+
+@Client.on_callback_query(filters.regex("start"))
+async def start(bot, msg, cb=True):   
+    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://t.me/Telugu_Bomma_VR_official>Telugu_Bomma_VR_official</a></b>"                                     
+    button= [[
+        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/Telugu_Bomma_VR_official")
+        ],[
+        InlineKeyboardButton("ℹ️ Help", callback_data="help"),
+        InlineKeyboardButton("📡 About", callback_data="about") 
+    ]]  
+    if cb:
+        await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
+    else:
+        await msg.reply_text(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
+
+
+@Client.on_callback_query(filters.regex("help"))
+async def help(bot, msg):
+    txt = "just send a file and /rename <new name> with replayed your file\n\n"
+    txt += "send photo to set thumbnail automatic \n"
+    txt += "/view to see your thumbnail \n"
+    txt += "/del to delete your thumbnail"
+    button= [[        
+        InlineKeyboardButton("🚫 Close", callback_data="del"),
+        InlineKeyboardButton("⬅️ Back", callback_data="start") 
+    ]]  
+    await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True)
+
+
+@Client.on_callback_query(filters.regex("about"))
+async def about(bot, msg):
+    me=await bot.get_me()
+    Master=f"<a href=https://t.me/Telugu_Bomma_VR_official</a>"  
+    Source="<a href=https://github.com/Nani0778/Simple-Rename-Bot>Click Here</a>"
+    txt=f"<b>Bot Name: {me.mention}\nDeveloper: <a href=https://github.com/Nani0778>Nani</a>\nBot Updates: <a href=https://t.me/Telugu_Bomma_VR_official>TELUGU_BOMMA_VR_OFFICIAL</a>\nMy Master's: {Master}\nSource Code: {Source}</b>"                 
+    button= [[        
+        InlineKeyboardButton("🚫 Close", callback_data="del"),
+        InlineKeyboardButton("⬅️ Back", callback_data="start") 
+    ]]  
+    await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
+
+
+@Client.on_callback_query(filters.regex("del"))
+async def closed(bot, msg):
+    try:
+        await msg.message.delete()
+    except:
+        return
